8f7b35f55da7e5fdf962dbb2b64cee24a53fbf54
Garegin866 <gareginharutyunyan866@gmail.com>
2025-10-30 08:38:24 +0400

    Implement Optional class and update main function to demonstrate usage

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+#include "optional.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    gx::Optional<int> opt = 42;
+    if (!opt.HasValue()) {
+        std::cout << "Optional is empty" << std::endl;
+    } else {
+        std::cout << "Optional has value: " << *opt << std::endl;
+    }
+
     return 0;
 }
